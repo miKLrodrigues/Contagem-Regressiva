6a1a08f9bba4bc2a8449e51ea9b95190d8f07b3e
Mikaele <mikaelerodrigues07@outlook.com>
2025-09-05 15:18:19 -0300

    contagem concluida

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Pressione qualquer tecla para começar...");
+        Console.ReadKey(true); // espera uma tecla
+
+        Console.WriteLine("Preparar...");
+        Console.ReadKey(true); // espera uma tecla
+
+        Console.WriteLine("Apontar...");
+        Console.ReadKey(true); // espera uma tecla
+
+        Console.WriteLine("FOGO!");
